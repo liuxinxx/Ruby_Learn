3bd391843fee6c0035f31f637127080cb9f23959
liuxin <727484328@qq.com>
2017-05-26 19:54:16 +0800

    逆序部分列

--- cctv/nx.rb
+++ cctv/nx.rb
@@ -1,12 +1,12 @@
 file = File.open('/media/liuxin/python/Ruby/RubymineProjects/test.txt')
 file.each_line {|line|
-  line = line.gsub("\n","")
+  line = line.gsub("\n", "")
   if line =="    "
     next
   end
   arr = line.split(',')
-str = ''
-arr[0], arr[1] = arr[1], arr[0]
-arr.each {|nn| str=str+nn+","}
-puts str.chop
+  str = ''
+  arr[0], arr[1] = arr[1], arr[0]
+  arr.each {|nn| str=str+nn+","}
+  puts str.chop
 }
