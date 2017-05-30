6c528cb66d3bbad69d844e69fe789441747f80f0
liuxin <727484328@qq.com>
2017-05-30 20:33:23 +0800

    添加下载网页方法

--- class_all.rb
+++ class_all.rb
@@ -2,21 +2,33 @@
 # require 'mechanize'
 # require 'uri/http'
 # require 'open-uri'
+require 'rubygems'
+require 'net/http'
+require 'open-uri'
 class DOW
   def download_html(url)
     begin
       agent = Mechanize.new
       agent.user_agent_alias = "Linux Mozilla"
-      # agent.open_timeout = 10
       # puts agent.methods
-      html = agent.get(url)
+      $proxy_addr = '113.69.38.106'
+      $proxy_port = '808'
+      # html = ''
+      # Net::HTTP.version_1_2
+      # Net::HTTP::Proxy($proxy_addr, $proxy_port).start(url) {|http|
+      # html= http.get('')
+      # str = html
+      # # str.encoding = 'gbk'
+      # puts str
+      # }
+       html = agent.get(url)
+      return html
+      # html = open(url).read
     rescue Exception=> e
       puts "出现异常:"+e.message
       puts e.backtrace
       puts e.class
       return "0"
     end
-    agent.history.clear
-    return html
   end
 end
