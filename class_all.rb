28a09525950e71bf945fed704761d5c19ff0c6ab
liuxin <727484328@qq.com>
2017-06-02 17:00:43 +0800

    添加两种下载网页王法

--- class_all.rb
+++ class_all.rb
@@ -1,29 +1,39 @@
 # encoding: utf-8
-# require 'mechanize'
-# require 'uri/http'
-# require 'open-uri'
+require 'mechanize'
 require 'rubygems'
 require 'net/http'
 require 'open-uri'
+require 'watir'
 class DOW
+  public
   def download_html(url)
     begin
       agent = Mechanize.new
       agent.user_agent_alias = "Linux Mozilla"
-      # puts agent.methods
-      $proxy_addr = '113.69.38.106'
-      $proxy_port = '808'
-      # html = ''
-      # Net::HTTP.version_1_2
-      # Net::HTTP::Proxy($proxy_addr, $proxy_port).start(url) {|http|
-      # html= http.get('')
-      # str = html
-      # # str.encoding = 'gbk'
-      # puts str
-      # }
        html = agent.get(url)
       return html
-      # html = open(url).read
+    rescue Exception=> e
+      puts "出现异常:"+e.message
+      puts e.backtrace
+      puts e.class
+      return "0"
+    end
+  end
+  def download(url)
+    b = Watir::Browser.new(:chrome)
+    # b = Watir::Browser.new(:phantomjs)
+    b.goto(url)
+    # b.close()
+    return  b.html
+  end
+  def download_proxy(url)
+    begin
+      $proxy_addr = '113.69.38.106'
+      $proxy_port = '808'
+      Net::HTTP.version_1_2
+      Net::HTTP::Proxy($proxy_addr, $proxy_port).start(url) {|http|
+      html= http.get('')}
+      return html
     rescue Exception=> e
       puts "出现异常:"+e.message
       puts e.backtrace
