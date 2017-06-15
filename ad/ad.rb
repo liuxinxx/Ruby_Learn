1d3112a1b77c78be5075968758cbd8a3b1b97f03
myvary <727484328@qq.com>
2017-06-15 18:28:11 +0800

    修改代码

--- ad/ad.rb
+++ ad/ad.rb
@@ -9,10 +9,11 @@ class AD
     @url_list = Array.new
     @number = 0
   end
+
   def root_url(url)
     flag = 1
     html = @dow.download_html(url)
-    root = url[0,url.rindex("/")+1]
+    root = url[0, url.rindex("/")+1]
     html.xpath('//div[@class="neiwen"]/*/tr').each do |div|
       pc = ''
       div.xpath('.//span').each do |sp|
@@ -20,7 +21,7 @@ class AD
           pc = $&
         else
           sp.xpath('a').each do |a|
-            a = [flag,pc,a.content,root+a['href']]
+            a = [flag, pc, a.content, root+a['href']]
             @url_list.push(a)
           end
         end
@@ -28,43 +29,68 @@ class AD
     end
     return @url_list
   end
+  def html_edcode(html)
+    html.xpath("//html").each do |h|
+      h.to_s.encode!('gb2312')
+      return h
+    end
+  end
+  def array_to_str(a)
+    str =''
+    a.each do |aa|
+      str+=aa.to_s+','
+    end
+    return str.chop
+  end
   def all_url
     page = []
+    file = File.open('./all.csv','ab+')
     while !@url_list.empty?
-      data = @url_list.shift#出队列一个
-      url = data[data.size-1]##取倒数第二个，倒数第二个存入的是链接
-      flag = data[0]##取标记
-      if flag!=2
-        data.pop##取出url
-        data.shift##取出标记
-        root = url[0,url.rindex("/")+1]##获取当前也的根目录
-        html = @dow.download_html(url)
+      data = @url_list.shift #出队列一个
+      url = data[data.size-1] ##取倒数第二个，倒数第二个存入的是链接
+      flag = data[0] ##取标记
+      # data.shift##取出标记
+      data.pop ##取出url
+      root = url[0, url.rindex("/")+1] ##获取当前也的根目录
+      html = @dow.download_html(url)
+      if html#获取网页
+        # html.encoding = 'gbk'
+        h = html_edcode(html)
 
-        print html.title
-        tds =''
-        html.xpath('//td').each do |td|
-        tds +=tds+ td.content.gsub(' ','').gsub("\n","")
-        end
-        p td
-
-        if /.导.师/ =~ td
+        if /.导.师/ =~ h
           data.unshift(2)
-          page =data+ [link.text,root+link.href]
-          @url_list.push(page)##将新对入队列
-          print @number, page
+          @number+=1
+          ssp=data+ ['第1页',url]
+          print @number, ssp
+          file.syswrite(@number.to_s+','+array_to_str(ssp)+"\r\n")
           puts
+          html.links.each do |link|
+            if link.text!='我要啦免费统计'and link.text!='第1页'
+                page =data+ [link.text, root+link.href]
+                # @url_list.push(page) ##将新对入队列
+                @number+=1
+                print @number, page
+                file.syswrite(@number.to_s+','+array_to_str(page)+"\r\n")
+                puts
+            end
+          end
+          next
+
+        end
+        if flag!=1
+          next
         else
           html.links.each do |link|
             if link.text =~/我要啦免费统计/
             else
-              @number+=1
-              data.unshift(1)
-              page =data+ [link.text,root+link.href]
-              @url_list.push(page)##将新对入队列
+              # @number+=1
+              page =data+ [link.text, root+link.href]
+              @url_list.push(page) ##将新对入队列
+              # print @number, page
+              # puts
             end
           end
         end
-
       end
     end
   end
