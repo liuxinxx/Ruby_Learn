abe66a4b064d8952c35faa99c3840f9d0c6d2afb
myvary <727484328@qq.com>
2017-11-01 15:01:52 +0800

    修改配置

--- ad/install_proxy.sh
+++ ad/install_proxy.sh
@@ -8,7 +8,7 @@ echo Y | sudo apt-get install apache2-utils
 cd /etc/squid3
 sudo rm -f squid3.conf
 curl -O 'https://raw.githubusercontent.com/myvary/Ruby_Learn/master/ad/squid.conf'
-sudo htpasswd  -c  /etc/squid/passwd  mc_proxy
+sudo htpasswd  -c  /etc/squid3/passwd  mc_proxy
 echo mancao
 echo mancao
 sudo squid3 -k reconfigur
